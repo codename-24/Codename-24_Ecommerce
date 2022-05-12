1f860fb52cdbecf8c8aa081d62c26cf8bcc8d0e2
codename-24 <102291062+codename-24@users.noreply.github.com>
2022-05-12 20:55:31 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,75 @@
+export const sliderItems = [
+    {
+      id: 1,
+      img: "https://i.ibb.co/cXFnLLV/3.png",
+      title: "SUMMER SALE",
+      desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+      bg: "fbf0f4",
+    },
+    {
+      id: 2,
+      img: "https://img.freepik.com/free-vector/3d-realistic-perfume-bottle_313044-64.jpg?w=2000",
+      title: "CognoScenti?",
+      desc: "SMELL IS A WORD, PERFUME IS LITERATURE",
+      bg: "fcf1ed",
+    },
+    {
+      id: 3,
+      img: "https://static.theceomagazine.net/wp-content/uploads/2020/05/22102234/panerai-nato-1-1100x733.jpg",
+      title: "Horologist!",
+      desc: "THE GREATEST GIFT YOU CAN GIVE TO SOMEONE IS YOUR TIME",
+      bg: "f3e9e2",
+    },
+  ];
+  export const categories = [
+    {
+      id: 1,
+      img: "https://images.pexels.com/photos/5886041/pexels-photo-5886041.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+      title: "SHIRT STYLE!",
+    },
+    {
+      id: 2,
+      img: "https://oracleoftime.com/wp-content/uploads/2020/07/The-Panerai-Experience-Interview-with-Jean-Marc-Pontrou%C3%A9.jpg",
+      title: "WATCH LOVE!",
+    },
+    {
+      id: 3,
+      img: "https://img.i-scmp.com/cdn-cgi/image/fit=contain,width=425,format=auto/sites/default/files/styles/768x768/public/d8/images/canvas/2021/12/03/3548e37e-02be-4563-86fd-eeb922c43d0e_94f7993f.jpg?itok=r0PvVNX8&v=1638518235",
+      title: "PERFUME",
+    },
+  ];
+
+  export const popularProducts = [
+    {
+      id:1,
+      img:"https://d3o2e4jr3mxnm3.cloudfront.net/Mens-Jake-Guitar-Vintage-Crusher-Tee_68382_1_lg.png",
+    },
+    {
+      id:2,
+      img:"https://hips.hearstapps.com/vader-prod.s3.amazonaws.com/1591135189-gardenia-les-exclusifs-de-chanel-parfum-grand-extrait-30-4fl-oz--packshot-default-120900-8804067016734.jpg",
+    },
+    {
+      id:3,
+      img:"https://www.prada.com/content/dam/pradanux_products/U/UCS/UCS319/1YOTF010O/UCS319_1YOT_F010O_S_182_SLF.png",
+    },
+    {
+      id:4,
+      img:"https://assets.xupes.com/Images/Product/34967/SingleImages/Main/Seamaster-007-James-Bond-Stainless-Steel-Gents-23230422101004.jpg",
+    },
+    {
+      id:5,
+      img:"https://images.ctfassets.net/5gvckmvm9289/3BlDoZxSSjqAvv1jBJP7TH/65f9a95484117730ace42abf64e89572/Noissue-x-Creatsy-Tote-Bag-Mockup-Bundle-_4_-2.png",
+    },
+    {
+      id:6,
+      img:"https://thumbs.dreamstime.com/b/diving-wrist-watch-white-background-cremona-italy-march-clockwork-panerai-c-company-based-milan-specialized-141849472.jpg",
+    },
+    {
+      id:7,
+      img:"https://media.allure.com/photos/5c8948cb6c85b02cef613c48/1:1/w_2000,h_2000,c_limit/dior-balade-sauvage.jpg",
+    },
+    {
+      id:8,
+      img:"https://www.pngarts.com/files/3/Women-Jacket-PNG-High-Quality-Image.png",
+    },
+  ]
