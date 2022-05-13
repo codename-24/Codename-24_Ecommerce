d20386ff4f84e223b1064d024de59e8dd3f35a38
codename-24 <vijay2495@gmail.com>
2022-05-13 22:19:39 +0530

    backend

---
+++ api/index.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+dotenv.config();
+const userRoute = require("./routes/user")
+const authRoute = require("./routes/auth")
+const productRoute = require("./routes/product")
+const cartRoute = require("./routes/cart")
+const orderRoute = require("./routes/order")
+mongoose.connect(process.env.MONGO_URL
+).then(()=>{
+    console.log("Database connected")
+}).catch((err)=>{
+    console.log(err)
+})
+app.use(cors());
+app.use(express.json());
+app.use("/api/auth",authRoute);
+app.use("/api/users",userRoute);
+app.use("/api/products",productRoute);
+app.use("/api/carts",cartRoute);
+app.use("/api/orders",orderRoute);
+
+
+
+app.listen(process.env.PORT || 5000, ()=>{
+    console.log("Server running 🚀");
+})
